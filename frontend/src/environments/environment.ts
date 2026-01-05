120e81f656f424c72b68a4416e0fca788cf19718
fabio <fabio.broccati@gmail.com>
2026-01-05 19:28:24 +0100

    Fix: sistemato l'URL in environment.prod.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  baseUrl: '' // Lasciare vuoto per usare il proxy locale
+  baseUrl: 'https://api-esame-fabio-bbd9ecafgzfefnhy.italynorth-01.azurewebsites.net' // Lasciare vuoto per usare il proxy locale
 };
